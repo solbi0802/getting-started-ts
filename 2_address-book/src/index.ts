63de8254a31c0fc9021beb17dea61131ade1147f
unknown <sbshin.dev@gmail.com>
2024-04-11 23:13:20 +0900

    [실습] 전화번호 관리 앱 타입 설정

---
+++ 2_address-book/src/index.ts
@@ -0,0 +1,105 @@
+/* eslint-disable prettier/prettier */
+
+interface PhoneNumberDictionary {
+  [phone: string]: {
+    num: number;
+  };
+}
+
+interface Contact {
+  name: string;
+  address: string;
+  phones: PhoneNumberDictionary;
+}
+
+enum PhoneType {
+  Home = "home",
+  Office = "office",
+  Studio = "studio",
+}
+
+// api
+function fetchContacts(): Promise<Contact[]> {
+  const contacts: Contact[] = [
+    {
+      name: "Tony",
+      address: "Malibu",
+      phones: {
+        home: {
+          num: 11122223333,
+        },
+        office: {
+          num: 44455556666,
+        },
+      },
+    },
+    {
+      name: "Banner",
+      address: "New York",
+      phones: {
+        home: {
+          num: 77788889999,
+        },
+      },
+    },
+    {
+      name: "마동석",
+      address: "서울시 강남구",
+      phones: {
+        home: {
+          num: 213423452,
+        },
+        studio: {
+          num: 314882045,
+        },
+      },
+    },
+  ];
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(contacts), 2000);
+  });
+}
+
+// main
+class AddressBook {
+  contacts: Contact[] = [];
+
+  constructor() {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    fetchContacts().then((response) => {
+      this.contacts = response;
+    });
+  }
+
+  findContactByName(name: string): Contact[] {
+    return this.contacts.filter((contact) => contact.name === name);
+  }
+
+  findContactByAddress(address: string): Contact[] {
+    return this.contacts.filter((contact) => contact.address === address);
+  }
+
+  findContactByPhone(phoneNumber: number, phoneType: PhoneType): Contact[] {
+    return this.contacts.filter(
+      (contact) => contact.phones[phoneType].num === phoneNumber
+    );
+  }
+
+  addContact(contact: Contact): void {
+    this.contacts.push(contact);
+  }
+
+  displayListByName(): string[] {
+    return this.contacts.map((contact) => contact.name);
+  }
+
+  displayListByAddress(): string[] {
+    return this.contacts.map((contact) => contact.address);
+  }
+  /* ------------------------------------------------ */
+}
+
+new AddressBook();
